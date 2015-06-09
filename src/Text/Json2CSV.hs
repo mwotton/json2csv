831569cb1c977cc5200054efeec09805e792c313
Mark Wotton <mwotton@gmail.com>
2015-06-09 17:18:47 +0700

    collapse the final level

--- src/Text/Json2CSV.hs
+++ src/Text/Json2CSV.hs
@@ -35,7 +35,11 @@ json2CSV :: Value -> [(Text, CVal)]
 json2CSV z = go z []
   where
     go (Object o) p   = concatMap (\(k, v) -> go v (k:p)) $ HM.toList o
-    go (Array a) p    = concat $ zipWith (\i v -> go v ((tshow i):p))  [(0::Int)..] $ V.toList a
+    go (Array a) p    = case mapM leaf (V.toList a) of
+      -- if we're at the final level, we can collapse with
+      Just x ->         [(mkPath p, CStr (T.intercalate "|" x))]
+      Nothing -> concat $ zipWith (\i v -> go v ((tshow i):p))  [(0::Int)..] $ V.toList a
+--     go (Array a) p    = concatMap (concat $ zipWith (\i v -> go v ((tshow i):p))  [(0::Int)..] $ V.toList a
     go (String s) p   = [(mkPath p, CStr s)]
     go (Number n) p   = [(mkPath p, CNumber n)]
     go (Bool True) p  = [(mkPath p, CStr "yes")] -- the stupid, it burns.
@@ -48,3 +52,12 @@ json2CSV z = go z []
 
 tshow :: Show a => a -> Text
 tshow = T.pack . show
+
+leaf :: Value -> Maybe Text
+leaf s = case s of
+  String str -> Just str
+  Number n   -> Just $ tshow n
+  Bool True   -> Just "yes"
+  Bool False  -> Just "no"
+  Null -> Just "null"
+  _ -> Nothing
