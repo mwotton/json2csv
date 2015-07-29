8bd3d45f10697aecc67ae4b1ef35ac26dead4a23
Mark Wotton <mwotton@gmail.com>
2015-07-29 09:42:05 -0400

    search one level on directories, closes #3

--- exec/json2csv.hs
+++ exec/json2csv.hs
@@ -3,18 +3,41 @@
 module Main where
 
 import           Control.Applicative  ((<$>))
+import           Control.Monad        (filterM)
 import           Data.Aeson
 import qualified Data.ByteString.Lazy as BL
 import           Data.Either          (partitionEithers)
+import           Data.Monoid          ((<>))
+import           Data.Monoid          (Monoid)
+import           Data.String          (IsString)
 import qualified Data.Text            as T
 import qualified Data.Text.IO         as TIO
+import           System.Directory
 import           System.Environment   (getArgs)
 import           Text.Json2CSV
 
+makeRelative :: (Monoid m, IsString m) => m -> m -> m
+makeRelative d f = d <> "/" <> f
+
+immediateDescendents :: FilePath -> IO [FilePath]
+immediateDescendents fp = do
+  isFile <- doesFileExist fp
+  if isFile
+    then return [fp]
+    else do
+      isDir <- doesDirectoryExist fp
+      if isDir
+        then filterM doesFileExist . map (makeRelative fp) =<< getDirectoryContents fp
+        else return []
+
+
+
 main :: IO ()
 main = do
-  (_bad,(good::[Value])) <- partitionEithers <$>
-                           (mapM (\x -> eitherDecode <$> BL.readFile x) =<< getArgs)
+  files <- concat <$> (mapM immediateDescendents =<< getArgs)
+  (_bad,(good::[Value])) <-
+    partitionEithers <$> mapM (\x -> eitherDecode <$> BL.readFile x) files
+
   let allRows = map (json2CSV defaults) good
       headers = getHeaders allRows
   TIO.putStrLn $ T.intercalate "," headers
