6d5ede1fb91fb6df07384a2107a4fe375b4c5f1b
Mark Wotton <mwotton@gmail.com>
2016-08-10 12:05:02 -0400

    expand hacks

--- exec/json2csv.hs
+++ exec/json2csv.hs
@@ -2,18 +2,19 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
-import           Control.Applicative  ((<$>))
-import           Control.Monad        (filterM)
+import           Control.Monad              (filterM)
+import           Control.Monad              (when)
 import           Data.Aeson
-import qualified Data.ByteString.Lazy as BL
-import           Data.Either          (partitionEithers)
-import           Data.Monoid          ((<>))
-import           Data.Monoid          (Monoid)
-import           Data.String          (IsString)
-import qualified Data.Text            as T
-import qualified Data.Text.IO         as TIO
+import qualified Data.ByteString.Lazy.Char8 as BL
+import           Data.Either                (partitionEithers)
+import           Data.Maybe                 (isJust, isNothing)
+import           Data.Monoid                ((<>))
+import           Data.String                (IsString)
+import qualified Data.Text                  as T
+import qualified Data.Text.IO               as TIO
+import qualified Data.Vector                as V
 import           System.Directory
-import           System.Environment   (getArgs)
+import           System.Environment         (getArgs, getEnvironment)
 import           Text.Json2CSV
 
 makeRelative :: (Monoid m, IsString m) => m -> m -> m
@@ -33,14 +34,35 @@ immediateDescendents fp = do
 
 
 
-
+-- | Two separate modes
+--   If there are arguments passed, we process all of them.
+--   If there are no arguments, we assume we are just reading from stdin.
 main :: IO ()
 main = do
-  files <- concat <$> (mapM immediateDescendents =<< getArgs)
-  (_bad,(good::[Value])) <-
-    partitionEithers <$> mapM (\x -> eitherDecode <$> BL.readFile x) files
+  a <- getArgs
+  justlines <- isJust . lookup "JUST_LINES" <$> getEnvironment
+  printHeaders <- isNothing . lookup "NO_HEADERS" <$> getEnvironment
+  shouldExpand <- isJust . lookup "EXPAND" <$> getEnvironment
+  (contents :: [BL.ByteString]) <- case a of
+    [] -> (if justlines
+          then BL.lines
+          else return) <$> BL.getContents
+    fs -> mapM BL.readFile . concat =<< mapM immediateDescendents fs
 
+
+  let (_bad,(good'::[Value])) = partitionEithers $ map eitherDecode contents
+
+  -- if we define EXPAND, we assume the top level is an array and
+  -- expand it out.
+  let good =
+        if shouldExpand
+        then concatMap expand good'
+        else good'
   let allRows = map (json2CSV defaults) good
       headers = getHeaders allRows
-  TIO.putStrLn $ T.intercalate "," headers
+  when printHeaders $ TIO.putStrLn $ T.intercalate "," headers
   (`mapM_` allRows) (TIO.putStrLn . formatLine headers)
+
+expand :: Value -> [Value]
+expand (Array o) = V.toList o
+expand x = error (show x)
