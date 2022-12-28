062da6e8e8fa9b7fc783a901dae0d787134a8b5c
Mark Wotton <mwotton@gmail.com>
2022-12-28 16:43:55 +0700

    make main entry visible and usable as a library function

--- src/MainEntry.hs
+++ src/MainEntry.hs
@@ -1,91 +1,21 @@
 {-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TupleSections #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module MainEntry where
 
-import           Control.Monad              (filterM)
-import           Control.Monad              (when)
-import           Data.Aeson
 import qualified Data.ByteString.Lazy.Char8 as BL
-import           Data.Either                (partitionEithers)
 import           Data.Maybe                 (isJust, isNothing)
-import           Data.String                (IsString)
-import qualified Data.Text                  as T
-import qualified Data.Text.IO               as TIO
-import qualified Data.Vector                as V
-import           System.Directory
 import           System.Environment         (getArgs, getEnvironment)
 import           Text.Json2CSV
-import qualified Data.Csv as CSV
-import System.IO(hClose,utf8,hSetEncoding,stdin)
-import System.IO.Temp(withSystemTempFile)
-
-makeRelative :: (Monoid m, IsString m) => m -> m -> m
-makeRelative d f = d <> "/" <> f
-
-immediateDescendents :: FilePath -> IO [FilePath]
-immediateDescendents fp = do
-  isFile <- doesFileExist fp
-  if isFile
-    then return [fp]
-    else do
-      isDir <- doesDirectoryExist fp
-      if isDir
-        then filterM doesFileExist . map (makeRelative fp) =<< getDirectoryContents fp
-        else return []
 
 
--- | Two separate modes
---   If there are arguments passed, we process all of them.
---   If there are no arguments, we assume we are just reading from stdin.
 main :: IO ()
 main = do
   args <- getArgs
-  justlines <- isJust . lookup "JUST_LINES" <$> getEnvironment
+  justLines <- isJust . lookup "JUST_LINES" <$> getEnvironment
   printHeaders <- isNothing . lookup "NO_HEADERS" <$> getEnvironment
   shouldExpand <- isJust . lookup "EXPAND" <$> getEnvironment
-  -- consistentJSON <- isJust . lookup "CONSISTENT_JSON" <$> getEnvironment
-  -- if we are reading stdin, just copy it to a temporary file
-  withSystemTempFile "json2csv.json" $ \fp handle -> do
-    filepaths <- case args of
-      [] -> do
-        hSetEncoding stdin utf8
-        hSetEncoding handle utf8
-        BL.hPutStr handle =<< BL.getContents
-        hClose handle
-        pure [fp]
-      xs -> do
-        hClose handle
-        concat <$> mapM immediateDescendents xs
 
-    -- this looks weird, but it lets us read the same file in lazily twice.
-    headers <- getHeaders <$> readJSONObjects justlines shouldExpand filepaths
-
-    allRows <- readJSONObjects justlines shouldExpand filepaths
-    when printHeaders $ TIO.putStrLn $ T.intercalate "," headers
-    BL.putStr $ CSV.encode (map (flattenValues headers) allRows :: [Row])
-
-readJSONObjects :: Traversable t =>
-                  Bool -> Bool -> t FilePath -> IO [[(T.Text, CVal)]]
-readJSONObjects justlines shouldExpand filepaths = do
-  contents <- concatMap
-             (if justlines
-              then BL.lines
-              else pure)
-      <$> mapM BL.readFile filepaths
-
-
-  let (_bad,(good'::[Value])) = partitionEithers $ map eitherDecode contents
-
-  -- if we define EXPAND, we assume the top level is an array and
-  -- expand it out.
-  let good =
-        if shouldExpand
-        then concatMap expand good'
-        else good'
-  pure $ map (json2CSV defaults) good
-
-
-expand :: Value -> [Value]
-expand (Array o) = V.toList o
-expand x = error (show x)
+  output <- runConversion Config{..} args
+  BL.putStr $ BL.unlines output
--- src/Text/Json2CSV.hs
+++ src/Text/Json2CSV.hs
@@ -1,20 +1,40 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module Text.Json2CSV where
 
+
+import           System.Directory
+import Data.Text.Encoding(encodeUtf8)
 import           Data.Aeson
 import qualified Data.Aeson.KeyMap as KM
 import qualified Data.Aeson.Key as K
 
 --import qualified Data.HashMap.Strict as HM
 import           Data.List           (nub)
+import qualified Data.ByteString.Lazy.Char8 as BL
 -- import           Data.Monoid         ((<>))
 import           Data.Scientific
 import           Data.Text           (Text)
 import qualified Data.Text           as T
 import qualified Data.Vector         as V
-import qualified Data.Csv as CSV
+-- import qualified Data.Csv as CSV
 import Data.Maybe(fromMaybe)
+import           Data.String                (IsString)
+import System.IO(hClose,utf8,hSetEncoding,stdin)
+import qualified Data.Csv as CSV
+import System.IO.Temp(withSystemTempFile)
+import           Data.Either                (partitionEithers)
+
+import           Control.Monad              (filterM)
+
+data Config = Config
+  { justLines :: Bool
+  , printHeaders :: Bool
+  , shouldExpand :: Bool
+  }
 
 data CVal = CStr Text
           | CNumber Scientific
@@ -82,3 +102,76 @@ leaf config s
     Null -> Just $ nullValue config
     _ -> Nothing
   | otherwise = Nothing
+
+
+
+makeRelative :: (Monoid m, IsString m) => m -> m -> m
+makeRelative d f = d <> "/" <> f
+
+immediateDescendents :: FilePath -> IO [FilePath]
+immediateDescendents fp = do
+  isFile <- doesFileExist fp
+  if isFile
+    then return [fp]
+    else do
+      isDir <- doesDirectoryExist fp
+      if isDir
+        then filterM doesFileExist . map (makeRelative fp) =<< getDirectoryContents fp
+        else return []
+
+runConversion :: Config -> [FilePath] -> IO [BL.ByteString]
+runConversion Config{..} args = do
+
+  -- consistentJSON <- isJust . lookup "CONSISTENT_JSON" <$> getEnvironment
+  -- if we are reading stdin, just copy it to a temporary file
+  -- | Two separate modes
+  --   If there are arguments passed, we process all of them.
+  --   If there are no arguments, we assume we are just reading from stdin.
+  withSystemTempFile "json2csv.json" $ \fp handle -> do
+    filepaths <- case args of
+      [] -> do
+        hSetEncoding stdin utf8
+        hSetEncoding handle utf8
+        BL.hPutStr handle =<< BL.getContents
+        hClose handle
+        pure [fp]
+      xs -> do
+        hClose handle
+        concat <$> mapM immediateDescendents xs
+
+    -- this looks weird, but it lets us read the same file in lazily twice.
+    headers <- getHeaders <$> readJSONObjects justLines shouldExpand filepaths
+
+    allRows <- readJSONObjects justLines shouldExpand filepaths
+    let headerRow = if printHeaders
+          then [BL.fromStrict $ encodeUtf8 $ T.intercalate "," headers] :: [BL.ByteString]
+          else []
+
+    pure $ headerRow <> [CSV.encode (map (flattenValues headers) allRows :: [Row])]
+
+
+
+readJSONObjects :: Traversable t =>
+                  Bool -> Bool -> t FilePath -> IO [[(T.Text, CVal)]]
+readJSONObjects justlines shouldExpand filepaths = do
+  contents <- concatMap
+             (if justlines
+              then BL.lines
+              else pure)
+      <$> mapM BL.readFile filepaths
+
+
+  let (_bad,(good'::[Value])) = partitionEithers $ map eitherDecode contents
+
+  -- if we define EXPAND, we assume the top level is an array and
+  -- expand it out.
+  let good =
+        if shouldExpand
+        then concatMap expand good'
+        else good'
+  pure $ map (json2CSV defaults) good
+
+
+expand :: Value -> [Value]
+expand (Array o) = V.toList o
+expand x = error (show x)
